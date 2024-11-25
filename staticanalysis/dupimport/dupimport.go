afbe141df5271ac7bd330dd5edbf9de9a473b452
koga <koga@wh-plus.com>
2024-11-25 13:06:58 +0900

    feat:import文の重複検知ロジックを作成

--- staticanalysis/dupimport/dupimport.go
+++ staticanalysis/dupimport/dupimport.go
@@ -1,16 +1,14 @@
 package dupimport
 
 import (
-	"go/ast"
+	"strconv"
 
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/passes/inspect"
-	"golang.org/x/tools/go/ast/inspector"
 )
 
-const doc = "dupimport is ..."
+const doc = "dupimport finds duplicated imports in same file"
 
-// Analyzer is ...
 var Analyzer = &analysis.Analyzer{
 	Name: "dupimport",
 	Doc:  doc,
@@ -21,20 +19,19 @@ var Analyzer = &analysis.Analyzer{
 }
 
 func run(pass *analysis.Pass) (any, error) {
-	inspect := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
-
-	nodeFilter := []ast.Node{
-		(*ast.Ident)(nil),
-	}
-
-	inspect.Preorder(nodeFilter, func(n ast.Node) {
-		switch n := n.(type) {
-		case *ast.Ident:
-			if n.Name == "gopher" {
-				pass.Reportf(n.Pos(), "identifier is gopher")
+	for _, f := range pass.Files {
+		paths := map[string]bool{}
+		for _, ip := range f.Imports {
+			path, err := strconv.Unquote(ip.Path.Value)
+			if err != nil {
+				return nil, err
+			}
+			if paths[path] {
+				pass.Reportf(ip.Pos(), "%s is duplicated import", path)
+			} else {
+				paths[path] = true
 			}
 		}
-	})
-
+	}
 	return nil, nil
 }
