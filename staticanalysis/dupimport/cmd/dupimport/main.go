21e92491c384e1a55e840977bfb5e867bda1ae9b
koga <koga@wh-plus.com>
2024-11-25 12:08:36 +0900

    debug:静的解析エラーが出るコードを仕込む

--- staticanalysis/dupimport/cmd/dupimport/main.go
+++ staticanalysis/dupimport/cmd/dupimport/main.go
@@ -7,3 +7,9 @@ import (
 )
 
 func main() { unitchecker.Main(dupimport.Analyzer) }
+
+func f() {
+	// The pattern can be written in regular expression.
+	var gopher int // want "pattern"
+	print(gopher)  // want "identifier is gopher"
+}
