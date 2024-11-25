63afd3e7c9cb3e129ea8c75aa65610bd7e31242e
koga <koga@wh-plus.com>
2024-11-25 12:32:48 +0900

    fix:go testが通るようにテストデータを修正

--- staticanalysis/dupimport/testdata/src/a/a.go
+++ staticanalysis/dupimport/testdata/src/a/a.go
@@ -1,7 +1,12 @@
 package a
 
 func f() {
-	// The pattern can be written in regular expression.
-	var gopher int // want "pattern"
+	// チェックされるべき識別子名を使用しているため、この2行はDiagnosticが報告される。
+	// 報告をアサートするにはテストデータの該当行にwantで始まるコメントを書く。
+	// すると、want以降の""で括られている正規表現にマッチするDiagnosticが該当行で報告されているかチェックすることができる。
+	var gopher int // want "identifier is gopher"
 	print(gopher)  // want "identifier is gopher"
+
+	var Gopher int // OK
+	print(Gopher)  // OK
 }
