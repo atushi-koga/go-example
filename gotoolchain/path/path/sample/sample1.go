11aa2d92688f3ff3631730268fde4485f523056b
koga <koga@wh-plus.com>
2025-05-16 22:00:36 +0900

    doc:解説コメントを追加

---
+++ gotoolchain/path/path/sample/sample1.go
@@ -0,0 +1,7 @@
+package sample
+
+import "fmt"
+
+func sampleMethod() {
+	fmt.Println("Hello, World!")
+}
--- staticanalysis/restrict/restrict.go
+++ staticanalysis/restrict/restrict.go
@@ -1,6 +1,7 @@
 package restrict
 
 import (
+	"fmt"
 	"go/ast"
 	"golang.org/x/tools/go/analysis"
 )
@@ -13,21 +14,27 @@ var Analyzer = &analysis.Analyzer{
 }
 
 func run(pass *analysis.Pass) (interface{}, error) {
+	fmt.Println("----------start analysis----------")
+	// pass.Files に解析対象のソースコードファイルが格納されている
 	for _, file := range pass.Files {
+		fmt.Printf("package name: %s\n", file.Name.Name)
 		ast.Inspect(file, func(n ast.Node) bool {
 			// 関数呼び出しか？
+			// CallExpr:関数呼び出しを表す式ノード
 			callExpr, ok := n.(*ast.CallExpr)
 			if !ok {
 				return true
 			}
 
 			// 選択式（fmt.Println のような形式）か？
+			// *ast.SelectorExpr:フィールドやメソッドを参照する式
 			selector, ok := callExpr.Fun.(*ast.SelectorExpr)
 			if !ok {
 				return true
 			}
 
 			// パッケージ識別子を取得（fmt）
+			// *ast.Ident:識別子（変数名、関数名、型名など）を表すノード
 			pkgIdent, ok := selector.X.(*ast.Ident)
 			if !ok {
 				return true
